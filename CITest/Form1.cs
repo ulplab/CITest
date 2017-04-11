c1ef967b9ce496364389809d3b089011416da8ad
agagliardi <agagliardi@ulp.edu.ar>
2017-04-11 10:03:55 -0300

    Add Thread UI interaction

--- CITest/Form1.cs
+++ CITest/Form1.cs
@@ -6,15 +6,33 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace CITest
 {
     public partial class Form1 : Form
     {
+        Thread t;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t = new Thread(() =>
+            {
+                for (int i=0;i<10;i++)
+                {
+                    label1.Invoke((Action)(() =>
+                    {
+                        label1.Text = "Pasada: " + i.ToString();
+                    }));
+                    Thread.Sleep(200);
+                }
+            });
+            t.Start();
         }
     }
 }
